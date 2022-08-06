635a4695888eb033053120ba5ad51e44d35dc1cb
CpyRubens <joaorubens2500@hotmail.com>
2022-08-06 15:14:27 -0300

    Update to-do.route.js

--- src/routes/to-do.route.js
+++ src/routes/to-do.route.js
@@ -2,10 +2,10 @@ const route = require('express').Router();
 
 const controllerTodo = require('../controllers/to-do.controller');
 
-route.get('/', controllerTodo.findAllToDoController);
-route.get('/:id', controllerTodo.findByIdToDoController);
-route.post('/', controllerTodo.createToDoController);
-route.put('/:id', controllerTodo.updateToDoController);
-route.delete('/:id', controllerTodo.deleteToDoController);
+route.get('/all-itens', controllerTodo.findAllToDoController);
+route.get('/item/:id', controllerTodo.findByIdToDoController);
+route.post('/create-item', controllerTodo.createToDoController);
+route.put('/update-item/:id', controllerTodo.updateToDoController);
+route.delete('/delete-item/:id', controllerTodo.deleteToDoController);
 
 module.exports = route;
